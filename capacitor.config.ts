4b984f9357846c7d0b7e3a42f9209babe6532953
Sujan Chandra Ray <77579088+wdsujanray@users.noreply.github.com>
2026-07-19 22:42:53 +0530

    Delete capacitor.config.ts

--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'sucharai.com',
-  appName: 'SucharAI',
-  webDir: 'dist'
-};
-
-export default config;
